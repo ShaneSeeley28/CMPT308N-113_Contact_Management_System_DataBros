64ee9154f39b492c719103083d2eab12e80c67e0
ShaneSeeley28 <112397882+ShaneSeeley28@users.noreply.github.com>
2024-02-07 19:15:18 -0500

    Delete Phase_3 directory

--- Phase_3/SpatialIndex1.sql
+++
@@ -1,9 +0,0 @@
-create database if not exists phase03;
-use phase03;
-
-create table geom (g GEOMETRY NOT NULL SRID 4326, SPATIAL INDEX(g));
-
-describe geom;
-select * from geom;
-
-ALTER TABLE geom DROP INDEX g;
